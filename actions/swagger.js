dcd7d4d38d9dad30576217830ec7653bb0d2eacf
m4mike <michael.bentein@gmail.com>
2015-07-13 13:52:07 +0200

    gettting heroku to work

--- actions/swagger.js
+++ actions/swagger.js
@@ -4,7 +4,12 @@ exports.swagger = {
   outputExample: {},
   run: function(api, data, next) {
     // Built by the swagger initializer.
-    data.response = api.swagger.documentation;
+        data.response = api.swagger.documentation;
+        if (process.env.NODE_ENV == 'production') {
+           
+            data.response.host = "https://test-mbe.herokuapp.com";
+        }
+
     next();
   }
 };
