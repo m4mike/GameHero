2bd70131bed0c075724483d4cae889d4c4affb53
m4mike <michael.bentein@gmail.com>
2015-07-14 11:28:05 +0200

    npm start mofig

--- app.js
+++ app.js
@@ -21,7 +21,9 @@ serverDomain.run(function () {
     // server is created in the scope of serverDomain
    
     var api;
-
+    console.log('***********************************************')
+    console.log('* Starting actionHero Domain                  *')
+    console.log('***********************************************')
     actionhero.start(function (err, apiFromCallback) {
         if (err) { console.log(err); }
         api = apiFromCallback;
